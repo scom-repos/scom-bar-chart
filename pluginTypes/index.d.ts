9abb7c754a5741a5dd24744006dc49436894d55e
Bryan <bryan.dev@instajs.com>
2023-08-29 15:51:13 +0700

    Combine general settings and theme settings into a single Edit & fix decimal places

--- pluginTypes/index.d.ts
+++ pluginTypes/index.d.ts
@@ -144,35 +144,45 @@ declare module "@scom/scom-bar-chart/data.json.ts" {
 /// <amd-module name="@scom/scom-bar-chart/formSchema.ts" />
 declare module "@scom/scom-bar-chart/formSchema.ts" {
     export function getBuilderSchema(): {
-        general: {
-            dataSchema: {
-                type: string;
-                required: string[];
-                properties: {
-                    title: {
-                        type: string;
-                    };
-                    description: {
-                        type: string;
-                    };
+        dataSchema: {
+            type: string;
+            required: string[];
+            properties: {
+                darkShadow: {
+                    type: string;
+                };
+                fontColor: {
+                    type: string;
+                    format: string;
+                };
+                backgroundColor: {
+                    type: string;
+                    format: string;
+                };
+                height: {
+                    type: string;
+                };
+                title: {
+                    type: string;
+                };
+                description: {
+                    type: string;
                 };
             };
-            uiSchema: {
+        };
+        uiSchema: {
+            type: string;
+            elements: {
                 type: string;
-                elements: ({
+                label: string;
+                elements: {
                     type: string;
-                    scope: string;
-                    options?: undefined;
-                } | {
-                    type: string;
-                    scope: string;
-                    options: {
-                        detail: {
-                            type: string;
-                        };
-                    };
-                })[];
-            };
+                    elements: {
+                        type: string;
+                        scope: string;
+                    }[];
+                }[];
+            }[];
         };
         advanced: {
             dataSchema: {
@@ -305,144 +315,135 @@ declare module "@scom/scom-bar-chart/formSchema.ts" {
                 }[];
             };
         };
-        theme: {
-            dataSchema: {
-                type: string;
-                properties: {
-                    darkShadow: {
-                        type: string;
-                    };
-                    fontColor: {
-                        type: string;
-                        format: string;
-                    };
-                    backgroundColor: {
-                        type: string;
-                        format: string;
-                    };
-                    height: {
-                        type: string;
-                    };
-                };
-            };
-        };
     };
     export function getEmbedderSchema(): {
-        general: {
-            dataSchema: {
-                type: string;
-                properties: {
-                    title: {
-                        type: string;
-                        required: boolean;
-                    };
-                    description: {
-                        type: string;
-                    };
-                    options: {
-                        type: string;
-                        title: string;
-                        properties: {
-                            xColumn: {
+        dataSchema: {
+            type: string;
+            properties: {
+                darkShadow: {
+                    type: string;
+                };
+                fontColor: {
+                    type: string;
+                    format: string;
+                };
+                backgroundColor: {
+                    type: string;
+                    format: string;
+                };
+                height: {
+                    type: string;
+                };
+                title: {
+                    type: string;
+                    required: boolean;
+                };
+                description: {
+                    type: string;
+                };
+                options: {
+                    type: string;
+                    title: string;
+                    properties: {
+                        xColumn: {
+                            type: string;
+                            title: string;
+                            required: boolean;
+                            properties: {
+                                key: {
+                                    type: string;
+                                    required: boolean;
+                                };
+                                type: {
+                                    type: string;
+                                    enum: string[];
+                                    required: boolean;
+                                };
+                            };
+                        };
+                        yColumns: {
+                            type: string;
+                            title: string;
+                            required: boolean;
+                            items: {
                                 type: string;
-                                title: string;
-                                required: boolean;
+                            };
+                        };
+                        groupBy: {
+                            type: string;
+                        };
+                        stacking: {
+                            type: string;
+                        };
+                        legend: {
+                            type: string;
+                            title: string;
+                            properties: {
+                                show: {
+                                    type: string;
+                                };
+                                scroll: {
+                                    type: string;
+                                };
+                                position: {
+                                    type: string;
+                                    enum: string[];
+                                };
+                            };
+                        };
+                        showDataLabels: {
+                            type: string;
+                        };
+                        percentage: {
+                            type: string;
+                        };
+                        xAxis: {
+                            type: string;
+                            properties: {
+                                title: {
+                                    type: string;
+                                };
+                                tickFormat: {
+                                    type: string;
+                                };
+                                reverseValues: {
+                                    type: string;
+                                };
+                            };
+                        };
+                        yAxis: {
+                            type: string;
+                            properties: {
+                                title: {
+                                    type: string;
+                                };
+                                tickFormat: {
+                                    type: string;
+                                };
+                                labelFormat: {
+                                    type: string;
+                                };
+                                position: {
+                                    type: string;
+                                    enum: string[];
+                                };
+                            };
+                        };
+                        seriesOptions: {
+                            type: string;
+                            items: {
+                                type: string;
                                 properties: {
                                     key: {
                                         type: string;
                                         required: boolean;
                                     };
-                                    type: {
-                                        type: string;
-                                        enum: string[];
-                                        required: boolean;
-                                    };
-                                };
-                            };
-                            yColumns: {
-                                type: string;
-                                title: string;
-                                required: boolean;
-                                items: {
-                                    type: string;
-                                };
-                            };
-                            groupBy: {
-                                type: string;
-                            };
-                            stacking: {
-                                type: string;
-                            };
-                            legend: {
-                                type: string;
-                                title: string;
-                                properties: {
-                                    show: {
-                                        type: string;
-                                    };
-                                    scroll: {
-                                        type: string;
-                                    };
-                                    position: {
-                                        type: string;
-                                        enum: string[];
-                                    };
-                                };
-                            };
-                            showDataLabels: {
-                                type: string;
-                            };
-                            percentage: {
-                                type: string;
-                            };
-                            xAxis: {
-                                type: string;
-                                properties: {
                                     title: {
                                         type: string;
                                     };
-                                    tickFormat: {
+                                    color: {
                                         type: string;
-                                    };
-                                    reverseValues: {
-                                        type: string;
-                                    };
-                                };
-                            };
-                            yAxis: {
-                                type: string;
-                                properties: {
-                                    title: {
-                                        type: string;
-                                    };
-                                    tickFormat: {
-                                        type: string;
-                                    };
-                                    labelFormat: {
-                                        type: string;
-                                    };
-                                    position: {
-                                        type: string;
-                                        enum: string[];
-                                    };
-                                };
-                            };
-                            seriesOptions: {
-                                type: string;
-                                items: {
-                                    type: string;
-                                    properties: {
-                                        key: {
-                                            type: string;
-                                            required: boolean;
-                                        };
-                                        title: {
-                                            type: string;
-                                        };
-                                        color: {
-                                            type: string;
-                                            format: string;
-                                        };
+                                        format: string;
                                     };
                                 };
                             };
@@ -451,26 +452,32 @@ declare module "@scom/scom-bar-chart/formSchema.ts" {
                 };
             };
         };
-        theme: {
-            dataSchema: {
+        uiSchema: {
+            type: string;
+            elements: {
                 type: string;
-                properties: {
-                    darkShadow: {
+                label: string;
+                elements: {
+                    type: string;
+                    elements: ({
                         type: string;
-                    };
-                    fontColor: {
+                        scope: string;
+                        elements?: undefined;
+                    } | {
                         type: string;
-                        format: string;
-                    };
-                    backgroundColor: {
-                        type: string;
-                        format: string;
-                    };
-                    height: {
-                        type: string;
-                    };
-                };
-            };
+                        elements: {
+                            type: string;
+                            scope: string;
+                            options: {
+                                detail: {
+                                    type: string;
+                                };
+                            };
+                        }[];
+                        scope?: undefined;
+                    })[];
+                }[];
+            }[];
         };
     };
 }
@@ -510,7 +517,7 @@ declare module "@scom/scom-bar-chart/dataOptionsForm.tsx" {
 }
 /// <amd-module name="@scom/scom-bar-chart" />
 declare module "@scom/scom-bar-chart" {
-    import { Module, ControlElement, Container, IDataSchema, VStack } from '@ijstech/components';
+    import { Module, ControlElement, Container, IDataSchema, VStack, IUISchema } from '@ijstech/components';
     import { IBarChartConfig } from "@scom/scom-bar-chart/global/index.ts";
     interface ScomBarChartElement extends ControlElement {
         lazyLoad?: boolean;
@@ -553,22 +560,7 @@ declare module "@scom/scom-bar-chart" {
                     redo: () => void;
                 };
                 userInputDataSchema: IDataSchema;
-                userInputUISchema: {
-                    type: string;
-                    elements: ({
-                        type: string;
-                        scope: string;
-                        options?: undefined;
-                    } | {
-                        type: string;
-                        scope: string;
-                        options: {
-                            detail: {
-                                type: string;
-                            };
-                        };
-                    })[];
-                };
+                userInputUISchema: IUISchema;
                 customUI?: undefined;
             } | {
                 name: string;
@@ -583,17 +575,6 @@ declare module "@scom/scom-bar-chart" {
                 };
                 userInputDataSchema?: undefined;
                 userInputUISchema?: undefined;
-            } | {
-                name: string;
-                icon: string;
-                command: (builder: any, userInputData: any) => {
-                    execute: () => Promise<void>;
-                    undo: () => void;
-                    redo: () => void;
-                };
-                userInputDataSchema: IDataSchema;
-                userInputUISchema?: undefined;
-                customUI?: undefined;
             })[];
             getData: any;
             setData: (data: IBarChartConfig) => Promise<void>;
@@ -613,22 +594,7 @@ declare module "@scom/scom-bar-chart" {
                     redo: () => void;
                 };
                 userInputDataSchema: IDataSchema;
-                userInputUISchema: {
-                    type: string;
-                    elements: ({
-                        type: string;
-                        scope: string;
-                        options?: undefined;
-                    } | {
-                        type: string;
-                        scope: string;
-                        options: {
-                            detail: {
-                                type: string;
-                            };
-                        };
-                    })[];
-                };
+                userInputUISchema: IUISchema;
                 customUI?: undefined;
             } | {
                 name: string;
@@ -643,17 +609,6 @@ declare module "@scom/scom-bar-chart" {
                 };
                 userInputDataSchema?: undefined;
                 userInputUISchema?: undefined;
-            } | {
-                name: string;
-                icon: string;
-                command: (builder: any, userInputData: any) => {
-                    execute: () => Promise<void>;
-                    undo: () => void;
-                    redo: () => void;
-                };
-                userInputDataSchema: IDataSchema;
-                userInputUISchema?: undefined;
-                customUI?: undefined;
             })[];
             getLinkParams: () => {
                 data: string;
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,8 @@ import {
   Panel,
   BarChart,
   moment,
-  Button
+  Button,
+  IUISchema
 } from '@ijstech/components';
 import { IBarChartConfig, callAPI, formatNumber, groupByCategory, extractUniqueTimes, concatUnique, groupArrayByKey, formatNumberByFormat, IBarChartOptions, isNumeric } from './global/index';
 import { chartStyle, containerStyle } from './index.css';
@@ -97,37 +98,56 @@ export default class ScomBarChart extends Module {
     this.onUpdateBlock();
   }
 
-  private _getActions(propertiesSchema: IDataSchema, themeSchema: IDataSchema, advancedSchema?: IDataSchema) {
+  private _getActions(dataSchema: IDataSchema, uiSchema: IUISchema, advancedSchema?: IDataSchema) {
     const builderSchema = getBuilderSchema();
     const actions = [
       {
-        name: 'General',
-        icon: 'cog',
+        name: 'Edit',
+        icon: 'edit',
         command: (builder: any, userInputData: any) => {
-          let _oldData: IBarChartConfig = DefaultData;
+          let oldData: IBarChartConfig = DefaultData;
+          let oldTag = {};
           return {
             execute: async () => {
-              _oldData = { ...this._data };
-              if (userInputData) {
-                if (advancedSchema) {
-                  this._data = { ...this._data, ...userInputData };
-                } else {
-                  this._data = { ...userInputData };
-                }
+              oldData = JSON.parse(JSON.stringify(this._data));
+              const {
+                title,
+                description,
+                options,
+                ...themeSettings
+              } = userInputData;
+
+              const generalSettings = {
+                title,
+                description,
+              };
+
+              if (advancedSchema) {
+                this._data = { ...this._data, ...generalSettings };
+              } else {
+                this._data = { ...generalSettings as IBarChartConfig, options };
               }
               if (builder?.setData) builder.setData(this._data);
               this.setData(this._data);
+
+              oldTag = JSON.parse(JSON.stringify(this.tag));
+              if (builder?.setTag) builder.setTag(themeSettings);
+              else this.setTag(themeSettings);
             },
             undo: () => {
-              if (advancedSchema) _oldData = { ..._oldData, options: this._data.options };
-              if (builder?.setData) builder.setData(_oldData);
-              this.setData(_oldData);
+              if (advancedSchema) oldData = { ...oldData, options: this._data.options };
+              if (builder?.setData) builder.setData(oldData);
+              this.setData(oldData);
+
+              this.tag = JSON.parse(JSON.stringify(oldTag));
+              if (builder?.setTag) builder.setTag(this.tag);
+              else this.setTag(this.tag);
             },
             redo: () => { }
           }
         },
-        userInputDataSchema: propertiesSchema,
-        userInputUISchema: advancedSchema ? undefined : builderSchema.general.uiSchema
+        userInputDataSchema: dataSchema,
+        userInputUISchema: uiSchema
       },
       {
         name: 'Data',
@@ -212,29 +232,6 @@ export default class ScomBarChart extends Module {
             return vstack;
           }
         }
-      },
-      {
-        name: 'Theme Settings',
-        icon: 'palette',
-        command: (builder: any, userInputData: any) => {
-          let oldTag = {};
-          return {
-            execute: async () => {
-              if (!userInputData) return;
-              oldTag = JSON.parse(JSON.stringify(this.tag));
-              if (builder?.setTag) builder.setTag(userInputData);
-              else this.setTag(userInputData);
-            },
-            undo: () => {
-              if (!userInputData) return;
-              this.tag = JSON.parse(JSON.stringify(oldTag));
-              if (builder?.setTag) builder.setTag(this.tag);
-              else this.setTag(this.tag);
-            },
-            redo: () => { }
-          }
-        },
-        userInputDataSchema: themeSchema
       }
     ]
     // if (advancedSchema) {
@@ -274,10 +271,10 @@ export default class ScomBarChart extends Module {
         target: 'Builders',
         getActions: () => {
           const builderSchema = getBuilderSchema();
-          const generalSchema = builderSchema.general.dataSchema as IDataSchema;
-          const themeSchema = builderSchema.theme.dataSchema as IDataSchema;
+          const dataSchema = builderSchema.dataSchema as IDataSchema;
+          const uiSchema = builderSchema.uiSchema as IUISchema;
           const advancedSchema = builderSchema.advanced.dataSchema as any;
-          return this._getActions(generalSchema, themeSchema, advancedSchema);
+          return this._getActions(dataSchema, uiSchema, advancedSchema);
         },
         getData: this.getData.bind(this),
         setData: async (data: IBarChartConfig) => {
@@ -292,9 +289,9 @@ export default class ScomBarChart extends Module {
         target: 'Embedders',
         getActions: () => {
           const embedderSchema = getEmbedderSchema();
-          const generalSchema = embedderSchema.general.dataSchema as any;
-          const themeSchema = embedderSchema.theme.dataSchema as IDataSchema;
-          return this._getActions(generalSchema, themeSchema)
+          const dataSchema = embedderSchema.dataSchema as any;
+          const uiSchema = embedderSchema.uiSchema as IUISchema;
+          return this._getActions(dataSchema, uiSchema);
         },
         getLinkParams: () => {
           const data = this._data || {};
